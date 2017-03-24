c17bd5cf117e73969f8c2c942a28cd598aa7c5de
wenchengye <wenchengye1005@163.com>
2017-03-24 13:36:30 +0800

    add : home navigate icons

--- app/src/main/java/com/heqi/kharazim/activity/VerticalType.java
+++ app/src/main/java/com/heqi/kharazim/activity/VerticalType.java
@@ -1,5 +1,7 @@
 package com.heqi.kharazim.activity;
 
+import com.heqi.kharazim.R;
+
 /**
  * Created by overspark on 2017/3/21.
  */
@@ -9,20 +11,20 @@ public enum VerticalType {
   Explore {
     @Override
     public int getIconRes() {
-      return 0;
+      return R.drawable.icon_home_navigate_explore;
     }
   },
 
   Archives {
     public int getIconRes() {
-      return 0;
+      return R.drawable.icon_home_navigate_archives;
     }
   },
 
   Settings {
     @Override
     public int getIconRes() {
-      return 0;
+      return R.drawable.icon_home_navigate_settings;
     }
   };
 
